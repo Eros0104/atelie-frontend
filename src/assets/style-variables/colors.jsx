91b54a218ba55096d018793245cc0e3839193794
Eros0104 <erosmendonca@outlook.com>
2021-03-05 12:45:29 -0300

    correção colors

--- src/assets/style-variables/colors.jsx
+++ src/assets/style-variables/colors.jsx
@@ -1,16 +1,7 @@
 const colors = {
   primary: "#e5a52c",
-  secondary: "#663399",
-  darkPrimary: "#c28717",
   white: "#FFFFFF",
-  darkGrey: "#555555",
-  grey: "#C5CFD6",
-  lightGrey: "#DDE3E9",
-  veryLightGrey: "#F2F2F2",
-  lightDanger: "#F59393",
-  danger: "#F33232",
   defaultFont: "#434445",
-  success: "#00C57C"
 };
 
 export default colors;
